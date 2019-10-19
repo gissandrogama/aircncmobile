4332925bbed41d95804f72144cd1b7e282d57759
gissandrogama <gissandrogama@gmail.com>
2019-10-19 02:34:06 -0300

    lista de spots pronta, falta efetuar o book

--- src/components/SpotList.js
+++ src/components/SpotList.js
@@ -1,11 +1,101 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Image, FlatList, View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+
+import api from '../services/api'
 
 export default function SpotList({ tech }){
+    const [spots, setSpots] = useState([])
+
+    useEffect(() => {
+        async function loadSpots() {
+            const response = await api.get('spots', {
+                params: { tech }
+            })
+            setSpots(response.data)
+        }
+
+        loadSpots()
+    }, [])
     return (
-        <View>
-            <Text>{tech}</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Empresas que usam <Text style={styles.bold}>{tech}</Text></Text>
+            <FlatList 
+                style={styles.list}
+                data={spots}
+                keyExtractor={ spot => spot._id}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                renderItem={({item}) => (
+                    <View style={styles.listItem}>
+                        <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url}} />
+                        <Text style={styles.company}>{item.company}</Text>
+                        <Text style={styles.price}>{item.price ? `R$ ${item.price}/dia` : 'Gratuito'}</Text>
+                        <TouchableOpacity onPress={() => {}} style={styles.button}>
+                            <Text style={styles.buttonText}>Solicitar reserva</Text>
+                        </TouchableOpacity>
+                    </View>
+                )}                
+            />
         </View>
 
     ) 
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 30,
+    },
+    
+    title: {
+        fontSize: 20,
+        color: '#444',
+        paddingHorizontal: 20,
+        marginBottom: 15,
+    },
+
+    bold: {
+        fontWeight: 'bold'
+    },
+
+    list: {
+        paddingHorizontal: 20,
+    },
+
+    listItem: {
+        marginRight: 15,
+    },
+
+    thumbnail: {
+        width: 200,
+        height: 120,
+        resizeMode: 'cover',
+        borderRadius: 2,
+    },
+
+    company: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#333',
+        marginTop: 10,
+    },
+
+    price: {
+        fontSize: 15,
+        color: '#999',
+        marginTop: 5,
+    },
+
+    button: {
+        height: 42,
+        backgroundColor: '#f05a5b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+    },
+
+    buttonText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 16,
+    }
+})
