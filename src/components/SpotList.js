bb1f7410992ded5d07ed099f946e2c3a5391f83c
gissandrogama <gissandrogama@gmail.com>
2019-10-19 01:35:24 -0300

    bug safeareaview e listando as tecnologias

---
+++ src/components/SpotList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function SpotList({ tech }){
+    return (
+        <View>
+            <Text>{tech}</Text>
+        </View>
+
+    ) 
+}
